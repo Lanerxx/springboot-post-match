7c7e807362d105ce2875ba580b6d050caac0bbf9
Lanerxx <892907311@qq.com>
2020-07-08 20:03:48 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootpostmatch/SpringbootPostMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpostmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPostMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootpostmatch/SpringbootPostMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpostmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
