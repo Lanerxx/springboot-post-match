77f1fda58ab7eda5139f7b9106166dabc17e2725
Lanerxx <892907311@qq.com>
2020-07-09 22:02:35 +0800

    student和enterprise信息更新service，利用vo解决枚举全后端传值的复杂性

--- src/main/java/com/example/springbootpostmatch/service/EnterpriseService.java
+++ src/main/java/com/example/springbootpostmatch/service/EnterpriseService.java
@@ -1,6 +1,9 @@
 package com.example.springbootpostmatch.service;
+import com.example.springbootpostmatch.component.vo.EnterpriseInoVo;
+import com.example.springbootpostmatch.component.vo.StudentInoVo;
 import com.example.springbootpostmatch.entity.Enterprise;
 import com.example.springbootpostmatch.entity.Post;
+import com.example.springbootpostmatch.entity.Student;
 import com.example.springbootpostmatch.repository.EnterpriseRepository;
 import com.example.springbootpostmatch.repository.PostRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +20,8 @@ public class EnterpriseService {
     @Autowired
     private EnterpriseRepository enterpriseRepository;
     @Autowired
+    private EnterpriseService enterpriseService;
+    @Autowired
     private PostRepository postRepository;
 
     //---------"Enterprise's CURD "-----------
@@ -32,6 +37,129 @@ public class EnterpriseService {
     public Enterprise updateEnterprise(Enterprise enterprise){
         enterpriseRepository.save(enterprise);
         return enterprise;
+    }
+    public Enterprise updateEnterprise(EnterpriseInoVo enterpriseInoVo,int eid){
+        Enterprise e = enterpriseService.getEnterprise(eid);
+        Enterprise enterprise = enterpriseInoVo.getEnterprise();
+        if (enterprise.getName() !=null){
+            e.setName(enterprise.getName());
+        }
+        if (enterprise.getDetail() !=null){
+            e.setDetail(enterprise.getDetail());
+        }
+        if (enterprise.getGraduationDate()!=null){
+            e.setGraduationDate(enterprise.getGraduationDate());
+        }
+        if (enterprise.getIndustry()!=null){
+            e.setIndustry(enterprise.getIndustry());
+        }
+        if (enterprise.getMajorCut() !=null){
+            e.setMajorCut(enterprise.getMajorCut());
+        }
+        if (enterprise.getLowestSalery()>0){
+            e.setLowestSalery(enterprise.getLowestSalery());
+        }
+        if (enterprise.getHighestSalery()>0){
+            e.setHighestSalery(enterprise.getHighestSalery());
+        }
+        if (enterprise.getLocation() !=null){
+            e.setLocation(enterprise.getLocation());
+        }
+        if (enterprise.getPhoneNumber() !=null){
+            e.setPhoneNumber(enterprise.getPhoneNumber());
+        }
+
+        if(enterpriseInoVo.getForeignLanguageProficiency()!=null){
+            switch(enterpriseInoVo.getForeignLanguageProficiency()) {
+                case "CET-6":
+                    e.setForeignLanguageProficiency(Student.ForeignLanguageProficiency.ENGLISH_CET_6);
+                    break;
+                case "CET-4":
+                    e.setForeignLanguageProficiency(Student.ForeignLanguageProficiency.ENGLISH_CET_4);
+                    break;
+                case "国外交流经验":
+                    e.setForeignLanguageProficiency(Student.ForeignLanguageProficiency.ENGLISH_Foreign_Exchange_Experience);
+                    break;
+                case "无":
+                    e.setForeignLanguageProficiency(Student.ForeignLanguageProficiency.NONE);
+                    break;
+            }
+        }
+
+        if (enterpriseInoVo.getGenderCut()!=null){
+            switch(enterpriseInoVo.getGenderCut()) {
+                case "男":
+                    e.setGenderCut(Student.Gender.MALE);
+                    break;
+                case "女":
+                    e.setGenderCut(Student.Gender.FEMALE);
+                    break;
+                case "无":
+                    e.setGenderCut(Student.Gender.NONE);
+                    break;
+            }
+        }
+
+        if (enterpriseInoVo.getSchoolRankCut()!=null){
+            switch(enterpriseInoVo.getSchoolRankCut()) {
+                case "985":
+                    e.setSchoolRankCut(Student.SchoolRank._985);
+                    break;
+                case "211":
+                    e.setSchoolRankCut(Student.SchoolRank._211);
+                    break;
+                case "省重点":
+                    e.setSchoolRankCut(Student.SchoolRank.PROVINCIAL_KEY);
+                    break;
+                case "普通本科":
+                    e.setSchoolRankCut(Student.SchoolRank.GENERAL_UNDERGRADUATE);
+                    break;
+                case "专科":
+                    e.setSchoolRankCut(Student.SchoolRank.JUNIOR_COLLEGE);
+                    break;
+                case "高职":
+                    e.setSchoolRankCut(Student.SchoolRank.HIGHER_VOCATIONAL_COLLEGE);
+                    break;
+                case "其他":
+                    e.setSchoolRankCut(Student.SchoolRank.OTHER);
+                    break;
+            }
+        }
+
+        if (enterpriseInoVo.getEducationCut()!=null){
+            switch(enterpriseInoVo.getEducationCut()) {
+                case "博士":
+                    e.setEducationCut(Student.Education.DOCTOR);
+                    break;
+                case "硕士":
+                    e.setEducationCut(Student.Education.MASTER);
+                    break;
+                case "本科":
+                    e.setEducationCut(Student.Education.BACHELOR);
+                    break;
+                case "高职高专":
+                    e.setEducationCut(Student.Education.HIGHER_VOCATIONAL_COLLEGE);
+                    break;
+                case "其他":
+                    e.setEducationCut(Student.Education.OTHER);
+                    break;
+            }
+        }
+        if (enterpriseInoVo.getEnterpriseNature()!=null){
+            switch (enterpriseInoVo.getEnterpriseNature()){
+                case "民企":
+                    e.setEnterpriseNature(Enterprise.EnterpriseNature.PRIVATE_ENTERPRISE);
+                    break;
+                case "外企":
+                    e.setEnterpriseNature(Enterprise.EnterpriseNature.FOREIGN);
+                    break;
+                case "国企":
+                    e.setEnterpriseNature(Enterprise.EnterpriseNature.STATE_OWNED);
+                    break;
+            }
+        }
+        enterpriseRepository.save(e);
+        return enterpriseInoVo.getEnterprise();
     }
 
     public Enterprise getEnterpriseByUserNumber(int number){
--- src/main/java/com/example/springbootpostmatch/service/StudentService.java
+++ src/main/java/com/example/springbootpostmatch/service/StudentService.java
@@ -1,11 +1,14 @@
 package com.example.springbootpostmatch.service;
+import com.example.springbootpostmatch.component.vo.StudentInoVo;
 import com.example.springbootpostmatch.entity.Student;
+import com.example.springbootpostmatch.entity.User;
 import com.example.springbootpostmatch.repository.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -14,6 +17,8 @@ import java.util.List;
 public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
+    private StudentService studentService;
 
     //---------"Student's CURD "-----------
     public Student addStudent(Student student){
@@ -29,7 +34,133 @@ public class StudentService {
         studentRepository.save(student);
         return student;
     }
+    public Student updateStudent(StudentInoVo studentInoVo, int sid){
+        Student s = studentService.getStudent(sid);
+        Student student = studentInoVo.getStudent();
+        if (student.getName() !=null){
+            s.setName(student.getName());
+        }
+        if (student.getBirthday() !=null){
+            s.setBirthday(student.getBirthday());
+        }
+        if (student.getGraduationSchoolName()!=null){
+            s.setGraduationSchoolName(student.getGraduationSchoolName());
+        }
+        if (student.getMajor()!=null){
+            s.setMajor(student.getMajor());
+        }
+        if (student.getGraduationDate() !=null){
+            s.setGraduationDate(student.getGraduationDate());
+        }
+        if (student.getNativePlace()!=null){
+            s.setNativePlace(student.getNativePlace());
+        }
+        if (student.getExpectedSalary()>0){
+            s.setExpectedSalary(student.getExpectedSalary());
+        }
+        if (student.getExpectedPosition() !=null){
+            s.setExpectedPosition(student.getExpectedPosition());
+        }
+        if (student.getExpectedIndustry() !=null){
+            s.setExpectedIndustry(student.getExpectedIndustry());
+        }
+        if (student.getEmploymentIntentionPlace()!=null){
+            s.setEmploymentIntentionPlace(student.getEmploymentIntentionPlace());
+        }
+        if (student.getPhoneNumber()!=null) {
+            s.setPhoneNumber(student.getPhoneNumber());
+        }
+        if (student.getMajorCourse() !=null){
+            s.setMajorCourse(student.getMajorCourse());
+        }
+        if (student.getSkill()!=null){
+            s.setSkill(student.getSkill());
+        }
+        if (student.getPaperCount()>0){
+            s.setPaperCount(student.getPaperCount());
+        }
+        if (student.getWorkExperience()>0){
+            s.setWorkExperience(student.getWorkExperience());
+        }
 
+        if (studentInoVo.getForeignLanguageProficiency()!=null){
+            switch(studentInoVo.getForeignLanguageProficiency()){
+                case "CET-6":
+                    s.setForeignlanguageproficiency(Student.ForeignLanguageProficiency.ENGLISH_CET_6);
+                    break;
+                case "CET-4":
+                    s.setForeignlanguageproficiency(Student.ForeignLanguageProficiency.ENGLISH_CET_4);
+                    break;
+                case "国外交流经验":
+                    s.setForeignlanguageproficiency(Student.ForeignLanguageProficiency.ENGLISH_Foreign_Exchange_Experience);
+                    break;
+                case "无":
+                    s.setForeignlanguageproficiency(Student.ForeignLanguageProficiency.NONE);
+                    break;
+            }
+
+        }
+        if (studentInoVo.getGender()!=null){
+            switch(studentInoVo.getGender()) {
+                case "男":
+                    s.setGender(Student.Gender.MALE);
+                    break;
+                case "女":
+                    s.setGender(Student.Gender.FEMALE);
+                    break;
+            }
+        }
+        if (studentInoVo.getSchoolRank()!=null){
+            switch(studentInoVo.getSchoolRank()) {
+                case "985":
+                    s.setGraduationSchoolRank(Student.SchoolRank._985);
+                    break;
+                case "211":
+                    s.setGraduationSchoolRank(Student.SchoolRank._211);
+                    break;
+                case "省重点":
+                    s.setGraduationSchoolRank(Student.SchoolRank.PROVINCIAL_KEY);
+                    break;
+                case "普通本科":
+                    s.setGraduationSchoolRank(Student.SchoolRank.GENERAL_UNDERGRADUATE);
+                    break;
+                case "专科":
+                    s.setGraduationSchoolRank(Student.SchoolRank.JUNIOR_COLLEGE);
+                    break;
+                case "高职":
+                    s.setGraduationSchoolRank(Student.SchoolRank.HIGHER_VOCATIONAL_COLLEGE);
+                    break;
+                case "其他":
+                    s.setGraduationSchoolRank(Student.SchoolRank.OTHER);
+                    break;
+            }
+        }
+        if (studentInoVo.getEducation()!=null){
+            switch(studentInoVo.getEducation()) {
+                case "博士":
+                    s.setEducation(Student.Education.DOCTOR);
+                    break;
+                case "硕士":
+                    s.setEducation(Student.Education.MASTER);
+                    break;
+                case "本科":
+                    s.setEducation(Student.Education.BACHELOR);
+                    break;
+                case "高职高专":
+                    s.setEducation(Student.Education.HIGHER_VOCATIONAL_COLLEGE);
+                    break;
+                case "其他":
+                    s.setEducation(Student.Education.OTHER);
+                    break;
+            }
+        }
+        studentRepository.save(s);
+        return s;
+    }
+
+    public Student getStudent(String name){
+        return studentRepository.findByName(name).orElse(null);
+    }
     public Student getStudentByUserNumber(int number){
         return studentRepository.getStudentsByUserNumber(number).orElse(null);
     }
