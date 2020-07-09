f78659b69989483da086d3e5243c7ada10940830
Lanerxx <892907311@qq.com>
2020-07-09 16:42:16 +0800

    新增user、student、admin CURD方法

--- src/main/java/com/example/springbootpostmatch/service/StudentService.java
+++ src/main/java/com/example/springbootpostmatch/service/StudentService.java
@@ -1,10 +1,43 @@
 package com.example.springbootpostmatch.service;
+import com.example.springbootpostmatch.entity.Student;
+import com.example.springbootpostmatch.repository.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @Transactional
 public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //---------"Student's CURD "-----------
+    public Student addStudent(Student student){
+        studentRepository.save(student);
+        return student;
+    }
+
+    public void deleteStudent(int id){
+        studentRepository.deleteById(id);
+    }
+
+    public Student updateStudent(Student student){
+        studentRepository.save(student);
+        return student;
+    }
+
+    public Student getStudentByUserNumber(int number){
+        return studentRepository.getStudentsByUserNumber(number).orElse(null);
+    }
+    public List<Student> listStudents() {
+        return studentRepository.findAll();
+    }
+    //Because the User and Student share the primary key，you can input an Id from the User as well as an Id from the Student
+    public Student getStudent(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
 }
--- src/main/java/com/example/springbootpostmatch/service/UserService.java
+++ src/main/java/com/example/springbootpostmatch/service/UserService.java
@@ -1,6 +1,12 @@
 package com.example.springbootpostmatch.service;
 
+import com.example.springbootpostmatch.entity.Admin;
+import com.example.springbootpostmatch.entity.User;
+import com.example.springbootpostmatch.repository.AdminRepository;
+import com.example.springbootpostmatch.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +14,43 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Transactional
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private PasswordEncoder encoder;
+    @Autowired
+    private UserService userService;
+
+
+    //---------"User's CURD "-----------
+    public User addUser(User user){
+        userRepository.save(user);
+        return user;
+    }
+    public User updateUserPassword(int id, String password){
+        User u = userService.getUserByID(id);
+        u.setPassword(encoder.encode(password));
+        userRepository.save(u);
+        return u;
+    }
+    public User getUserByID(int id){
+        return userRepository.findById(id).orElse(null);
+    }
+    public User getUserByNumber(String number) {
+        return  userRepository.findByNumber(number).orElse(null);
+    }
+    public User updateUser(User user){
+        userRepository.save(user);
+        return user;
+    }
+    //---------"Admin's CURD "-----------
+    public Admin addAdmin(Admin admin){
+        adminRepository.save(admin);
+        return admin;
+    }
+
+
+
 }
