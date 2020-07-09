d384bfbe0ceae6f0ce37e8ac49a3c01ffa1b0aa9
Lanerxx <892907311@qq.com>
2020-07-09 14:44:56 +0800

    拦截器按不同角色声明，按一定顺序注册

---
+++ src/main/java/com/example/springbootpostmatch/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package com.example.springbootpostmatch;
+
+import com.example.springbootpostmatch.intercepter.AdminInterceptor;
+import com.example.springbootpostmatch.intercepter.EnterpriseInterceptor;
+import com.example.springbootpostmatch.intercepter.LoginInterceptor;
+import com.example.springbootpostmatch.intercepter.StudentInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+    @Autowired
+    private EnterpriseInterceptor enterpriseInterceptor;
+    @Autowired
+    private StudentInterceptor studentInterceptor;
+    @Autowired
+    private AdminInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login");
+        registry.addInterceptor(enterpriseInterceptor)
+                .addPathPatterns("/api/tutor**");
+        registry.addInterceptor(studentInterceptor)
+                .addPathPatterns("/api/student**");
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/api/admin**");
+
+    }
+
+}
