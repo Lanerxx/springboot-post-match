cd11e4fabd174cba78b86b75cba1e62ca7f9225d
Lanerxx <892907311@qq.com>
2020-07-09 21:59:18 +0800

    studentController组件实现，学生主页，学生信息修改

--- src/main/java/com/example/springbootpostmatch/controller/StudentController.java
+++ src/main/java/com/example/springbootpostmatch/controller/StudentController.java
@@ -1,4 +1,62 @@
 package com.example.springbootpostmatch.controller;
 
+import com.example.springbootpostmatch.component.RequestComponent;
+import com.example.springbootpostmatch.component.vo.StudentInoVo;
+import com.example.springbootpostmatch.entity.Enterprise;
+import com.example.springbootpostmatch.entity.Student;
+import com.example.springbootpostmatch.service.EnterpriseService;
+import com.example.springbootpostmatch.service.StudentService;
+import com.example.springbootpostmatch.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/student/")
+@Slf4j
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @Autowired
+    private EnterpriseService enterpriseService;
+    @Autowired
+    private RequestComponent requestComponent;
+
+    @GetMapping("index")
+    public Map getIndex(){
+        Student student = studentService.getStudent(requestComponent.getUid());
+        List<Enterprise> enterprises = enterpriseService.listEnterprises();
+        return Map.of(
+                "student",student,
+                "enterprises", enterprises
+        );
+    }
+
+    @GetMapping("match")
+    public Map getMatch(){
+        Enterprise enterprise = enterpriseService.getEnterprise(requestComponent.getUid());
+        List<Student> students = studentService.listStudents();
+        return Map.of(
+                "enterprise", enterprise,
+                "students",students
+
+        );
+    }
+
+    @PatchMapping("information")
+    public Map updateStudentInformation(@RequestBody StudentInoVo studentInoVo){
+         studentService.updateStudent(studentInoVo, requestComponent.getUid());
+         Student student = studentService.getStudent(requestComponent.getUid());
+        return Map.of(
+                "student",student
+        );
+    }
+
+
+
+
 }
