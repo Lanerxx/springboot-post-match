ba8f0023a150b921e9952dae8e70f2869d9807f1
Lanerxx <892907311@qq.com>
2020-07-13 22:30:21 +0800

    创建studentMatchVo，封装学生和符合要求程度的加权值

---
+++ src/main/java/com/example/springbootpostmatch/component/vo/StudentMatchVo.java
@@ -0,0 +1,10 @@
+package com.example.springbootpostmatch.component.vo;
+
+import com.example.springbootpostmatch.entity.Student;
+import lombok.Data;
+
+@Data
+public class StudentMatchVo {
+    Student student;
+    float score;
+}
